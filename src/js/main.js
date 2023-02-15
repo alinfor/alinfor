cfff604a8b29f414c86df8be055d5adbf4e1660f
Alifor <116239087+alinfor@users.noreply.github.com>
2023-02-15 03:03:50 +0100

    refacto

--- src/js/main.js
+++ src/js/main.js
@@ -12,11 +12,9 @@ function init(){
     document.querySelector("#person-fullname").innerHTML = "Ali Abdalla"
     document.querySelector("#person-firstname").innerHTML = "Ali";
     document.querySelector("#person-job").innerHTML = "Développeur web";
-    document.querySelector("#person-position").innerHTML = "Développeur web";
+    document.querySelector("#person-position").innerHTML = "Développeur front-end";
     document.querySelector("#person-company").innerHTML = React+" chez en recherche d'emploi";
     document.querySelector("#person-city").innerHTML = Puteaux+ ", " + 92800;
-    document.querySelector("#person-speciality").innerHTML = "React/Vue";
-    document.querySelector("#person-speciality").innerHTML = "React";
     document.querySelector("#person-email").href = "mailto:"+"ali.abdalla@outlook.fr";
 }
 
